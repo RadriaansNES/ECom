817cd82ecc7f0cd099f545ae25c0fc3424a49dfc
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-08-25 15:32:32 -0400

    Modularity.

--- routing/routing.js
+++ routing/routing.js
@@ -1,24 +1,22 @@
-require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const express = require('express');
-const app = express();
 const pricingController = require('./controller/pricingController');
 
+module.exports = (app) => {
+    //THIS IS THE ROUTE!
+    app.post('/create-checkout-session', async (req, res) => {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+                {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    price: await pricingController.createPrice(),
+                    quantity: 1,
+                },
+            ],
+            mode: 'payment',
+            success_url: 'https://www.google.ca',
+            cancel_url: 'https://www.facebook.com',
+        });
 
-//THIS IS THE ROUTE!
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                price: await pricingController.createPrice(),
-                quantity: 1,
-            },
-        ],
-        mode: 'payment',
-        success_url: 'https://www.google.ca',
-        cancel_url: 'https://www.facebook.com',
+        res.redirect(303, session.url);
     });
-
-    res.redirect(303, session.url);
-});
+};
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-const PORT = process.env.PORT || 4242;
+require('./routing')(app); // Import the routing file and pass the app instance
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 4242;
+
 app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
