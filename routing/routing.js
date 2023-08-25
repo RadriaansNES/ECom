3a482673e63a70f02b5beb20fb37b84897707c48
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-08-25 16:01:52 -0400

    Final fix server.

--- routing/routing.js
+++ routing/routing.js
@@ -3,22 +3,22 @@ const pricingController = require('../controller/pricingController');
 const express = require('express');
 const router = express.Router();
 
-module.exports = (router) => {
-    //THIS IS THE ROUTE!
-    router.post('/create-checkout-session', async (req, res) => {
-        const session = await stripe.checkout.sessions.create({
-            line_items: [
-                {
-                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    price: await pricingController.createPrice(),
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
-            success_url: 'https://www.google.ca',
-            cancel_url: 'https://www.facebook.com',
-        });
 
-        res.redirect(303, session.url);
-    });
-};
+router.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: await pricingController.createPrice(),
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://www.google.ca',
+    cancel_url: 'https://www.facebook.com',
+  });
+
+  res.redirect(303, session.url);
+});
+
+module.exports = router;
