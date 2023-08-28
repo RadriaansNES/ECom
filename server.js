ee239844426dad730c5fe1c318dda38afd42c5f8
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-08-28 13:43:30 -0400

    Setup for web server.

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const app = express(); // Initialize the Express app instance
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 4242;
 
 // Import the routing file and pass the app instance
 require('./routing/routing')(app);
+
+// Define a route handler for the root path ("/")
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/view/index.html'); // Adjust the file path as needed
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
