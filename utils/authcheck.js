f11944060613af826986bbc814fc426e04309af8
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-11-03 17:15:51 -0400

    Pre testimonial page spread/fix.

--- utils/authcheck.js
+++ utils/authcheck.js
@@ -1,7 +1,17 @@
-  fetch('/check-auth') 
-    .then(response => {
-      if (response.status === 200) {
-      } else {
-        window.location.href = '/';
-      }
-    });
+function updateHeader(authenticated) {
+  const loginLink = document.querySelector('.navbar-brand[href="/account"]');
+  if (authenticated) {
+    loginLink.textContent = 'Account';
+  } else {
+    loginLink.textContent = 'Login';
+  }
+}
+
+fetch('/check-auth')
+  .then((response) => {
+    if (response.status === 200) {
+      updateHeader(true);
+    } else {
+      updateHeader(false);
+    }
+  });
